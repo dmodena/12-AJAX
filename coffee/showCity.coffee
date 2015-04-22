21b19e72d870e209d624634c5bdc9953abf4af63
Douglas Modena <d.modena@live.com>
2015-04-21 21:49:16 -0500

    Function display city generating output.

--- coffee/showCity.coffee
+++ coffee/showCity.coffee
@@ -1,2 +1,57 @@
 displayCity = (ajax) ->
-  alert "Hi"
+  # Getting value of selected city
+  cityOutput = document.getElementById "container"
+  citySelect = document.getElementById "city"
+  chosenCity = citySelect.value
+
+  # Getting document as XML
+  xmlDoc = ajax.responseXML
+
+  # Saving elements in variables
+  cities = xmlDoc.getElementsByTagName "city"
+
+  # Finding chosen element
+  for city in cities
+    # Creating collections
+    names = city.getElementsByTagName "name"
+    temperatures = city.getElementsByTagName "temperature"
+    winds = city.getElementsByTagName "wind"
+    humidities = city.getElementsByTagName "humidity"
+    pressures = city.getElementsByTagName "pressure"
+    visibilities = city.getElementsByTagName "visibility"
+
+    # Copying content in variables
+    name = names[0]
+    temperature = temperatures[0]
+    wind = winds[0]
+    humidity = humidities[0]
+    pressure = pressures[0]
+    visibility = visibilities[0]
+
+    # If element is found
+    if name.childNodes[0].textContent is chosenCity
+      tempP = document.createElement "p"
+      windP = document.createElement "p"
+      humiP = document.createElement "p"
+      presP = document.createElement "p"
+      visiP = document.createElement "p"
+
+      # Creating output
+      textN = "Temperature: " + temperature.childNodes[0].textContent
+      tempP.innerHTML = textN
+      textN = "Wind: " + wind.childNodes[0].textContent
+      windP.innerHTML = textN
+      textN = "Humidity: " + humidity.childNodes[0].textContent
+      humiP.innerHTML = textN
+      textN = "Pressure: " + pressure.childNodes[0].textContent
+      presP.innerHTML = textN
+      textN = "Visibility: " + visibility.childNodes[0].textContent
+      visiP.innerHTML = textN
+
+  # Adding output
+  cityOutput.appendChild tempP
+  cityOutput.appendChild windP
+  cityOutput.appendChild humiP
+  cityOutput.appendChild presP
+  cityOutput.appendChild visiP
+  return
--- public/js/showCity.js
+++ public/js/showCity.js
@@ -1,5 +1,47 @@
 var displayCity;
 
 displayCity = function(ajax) {
-  return alert("Hi");
+  var chosenCity, cities, city, cityOutput, citySelect, humiP, humidities, humidity, i, len, name, names, presP, pressure, pressures, tempP, temperature, temperatures, textN, visiP, visibilities, visibility, wind, windP, winds, xmlDoc;
+  cityOutput = document.getElementById("container");
+  citySelect = document.getElementById("city");
+  chosenCity = citySelect.value;
+  xmlDoc = ajax.responseXML;
+  cities = xmlDoc.getElementsByTagName("city");
+  for (i = 0, len = cities.length; i < len; i++) {
+    city = cities[i];
+    names = city.getElementsByTagName("name");
+    temperatures = city.getElementsByTagName("temperature");
+    winds = city.getElementsByTagName("wind");
+    humidities = city.getElementsByTagName("humidity");
+    pressures = city.getElementsByTagName("pressure");
+    visibilities = city.getElementsByTagName("visibility");
+    name = names[0];
+    temperature = temperatures[0];
+    wind = winds[0];
+    humidity = humidities[0];
+    pressure = pressures[0];
+    visibility = visibilities[0];
+    if (name.childNodes[0].textContent === chosenCity) {
+      tempP = document.createElement("p");
+      windP = document.createElement("p");
+      humiP = document.createElement("p");
+      presP = document.createElement("p");
+      visiP = document.createElement("p");
+      textN = "Temperature: " + temperature.childNodes[0].textContent;
+      tempP.innerHTML = textN;
+      textN = "Wind: " + wind.childNodes[0].textContent;
+      windP.innerHTML = textN;
+      textN = "Humidity: " + humidity.childNodes[0].textContent;
+      humiP.innerHTML = textN;
+      textN = "Pressure: " + pressure.childNodes[0].textContent;
+      presP.innerHTML = textN;
+      textN = "Visibility: " + visibility.childNodes[0].textContent;
+      visiP.innerHTML = textN;
+    }
+  }
+  cityOutput.appendChild(tempP);
+  cityOutput.appendChild(windP);
+  cityOutput.appendChild(humiP);
+  cityOutput.appendChild(presP);
+  cityOutput.appendChild(visiP);
 };
